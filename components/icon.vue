6a1829cccbf4c8db40c1919dd03010a335ad4348
Andrej Sharapov <an.sharapov90@ya.ru>
2022-02-19 22:31:49 +0300

    added @mdi/js

---
+++ components/icon.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+svg(viewBox="0 0 24 24", :width="width", :height="height")
+  path(:d="icon", fill="currentColor")
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    icon: null,
+    width: {
+      type: String,
+      default: "24",
+    },
+    height: {
+      type: String,
+      default: "24",
+    },
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,32 @@
   global-color-mode-picker
   hello-world.mb-4.text-center(:msg="title")
   p Added: {{ desc }}
+  .flex
+    a.mx-2.text-gray-400(
+      class="hover:text-gray-700",
+      target="_blank",
+      rel="noopener noreferrer",
+      href="https://github.com/andrejsharapov/nuxt3-app",
+      title="nuxt3-app"
+    )
+      icon(:icon="github")
+    a.mx-2.text-gray-400(
+      class="hover:text-gray-700",
+      target="_blank",
+      rel="noopener noreferrer",
+      href="https://twitter.com/andrejsharapov/status/1494724339023106050",
+      title="twitter"
+    )
+      icon(:icon="twitter")
 </template>
 
 <script lang="ts" setup>
+import { mdiGithub, mdiTwitter } from "@mdi/js";
+
 const title = "Nuxt 3 + Tailwind 3";
 const desc = "Pug + S?(C|a)SS";
+const github = mdiGithub;
+const twitter = mdiTwitter;
 
 definePageMeta({
   layout: "default",
